52d69fc801e65d6dd3f34572a73cbc54a26ec311
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-07-05 17:34:20 -0400

    wip

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirbnbAid::Application.config.secret_key_base = '493066eeadc693befc82b41de10d0b6f51b277b3d44f664b5cd92313bbe77b55c71d23263f073150408feb197a8191713345d9f3ceb4854b5da45a834696ce65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AirbnbAid::Application.config.session_store :cookie_store, key: '_airbnb-aid_session'
