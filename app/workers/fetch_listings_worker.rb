936151e955b403c57ea96904119313de9bd595a1
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-10-13 22:50:19 -0400

    wip

--- app/workers/fetch_listings_worker.rb
+++ app/workers/fetch_listings_worker.rb
@@ -3,27 +3,24 @@ class FetchListingsWorker
 
   def perform(params)
     Airbnb::Listing.fetch(params).each do |airbnb_listing|
-      listing = Listing.find_or_create_by({
-        :airbnb_id => airbnb_listing['id']
-      })
+      listing = Listing.find_or_create_by({ :airbnb_id => airbnb_listing.id })
       listing.update_attributes!({
-        :name                => airbnb_listing['name'],
-        :city                => airbnb_listing['city'],
-        :latitude            => airbnb_listing['lat'],
-        :longitude           => airbnb_listing['lng'],
-        :country             => airbnb_listing['country'],
-        :smart_location      => airbnb_listing['smart_location'],
-        :bedrooms            => airbnb_listing['bedrooms'],
-        :beds                => airbnb_listing['beds'],
-        :neighborhood        => airbnb_listing['neighborhood'],
-        :person_capacity     => airbnb_listing['person_capacity'],
-        :state               => airbnb_listing['state'],
-        :zipcode             => airbnb_listing['zipcode'],
-        :address             => airbnb_listing['address'],
-        :country_code        => airbnb_listing['country_code'],
-        :cancellation_policy => airbnb_listing['cancellation_policy'],
-        :property_type       => airbnb_listing['property_type'],
-        :room_type           => airbnb_listing['room_type']
+        :name                => airbnb_listing.name,
+        :city                => airbnb_listing.city,
+        :latitude            => airbnb_listing.lat,
+        :longitude           => airbnb_listing.lng,
+        :smart_location      => airbnb_listing.smart_location,
+        :bedrooms            => airbnb_listing.bedrooms,
+        :beds                => airbnb_listing.beds,
+        :neighborhood        => airbnb_listing.neighborhood,
+        :person_capacity     => airbnb_listing.person_capacity,
+        :state               => airbnb_listing.state,
+        :zipcode             => airbnb_listing.zipcode,
+        :address             => airbnb_listing.address,
+        :country_code        => airbnb_listing.country_code,
+        :cancellation_policy => airbnb_listing.cancellation_policy,
+        :property_type       => airbnb_listing.property_type,
+        :room_type           => airbnb_listing.room_type
       })
     end
   end
