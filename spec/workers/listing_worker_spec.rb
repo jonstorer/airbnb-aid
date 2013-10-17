d09c633883da0260c60460a942fa558398b6c289
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-10-16 23:36:28 -0400

    more

--- spec/workers/listing_worker_spec.rb
+++ spec/workers/listing_worker_spec.rb
@@ -13,13 +13,22 @@ describe ListingWorker, '#perform, it updated the record from airbnb' do
   let(:builder)        { double(:save! => true) }
   let(:airbnb_listing) { double('airbnb_listing') }
 
+  before do
+    Listing.stub(:find => listing)
+    ListingUpdater.stub(:new => builder)
+  end
+
   it 'should update the listing with the airbnb record' do
-    Sidekiq::Testing.fake! do
-      Airbnb::Listing.stub(:find => airbnb_listing)
-      ListingUpdater.stub(:new => builder)
-      ListingWorker.new.perform(listing.id)
-      ListingUpdater.should have_received(:new).with(listing, airbnb_listing)
-      builder.should have_received(:save!)
-    end
+    Airbnb::Listing.stub(:find => airbnb_listing)
+    ListingWorker.new.perform(listing.id)
+    ListingUpdater.should have_received(:new).with(listing, airbnb_listing)
+    builder.should have_received(:save!)
+  end
+
+  it 'should update the listing with the airbnb record' do
+    Airbnb::Listing.stub(:find) { raise Airbnb::RecordNotFound }
+    listing.stub(:update_attributes! => true)
+    ListingWorker.new.perform(listing.id)
+    listing.should have_received(:update_attributes!).with({:active => false})
   end
 end
